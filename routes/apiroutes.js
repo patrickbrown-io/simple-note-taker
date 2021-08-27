c50a6cd94d3e5aa86b82f8026c0d97b6e3d7678c
Patrick Brown <breepatbree@gmail.com>
2021-08-27 16:44:12 -0600

    POST works, need to modularize

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -3,8 +3,31 @@ const data = require('../db/db.json');
 const express = require('express');
 const app = express();
 
-app.get('/api/notes', (req,res) =>
-    res.status(200).json(data));
+// // POST request to add a note
+// app.post('/api/notes', (req, res) => {
+//   // Log that a POST request was received
+//   console.log(req.body);
+//   console.info(`${req.method} request received to add a note`);
+// //     // Obtain existing reviews
+//     fs.readFile(data, 'utf8', (err, data) => {
+//       if (err) {
+//         console.error(err);
+//       } else {
+//         // Convert string into JSON object
+//         const parsedNotes = JSON.parse(data);
 
-module.exports = router;
+//         // Add a new Note
+//         parsedNotes.push(newReview);
 
+//         // Write updated reviews back to the file
+//         fs.writeFile(
+//           data,
+//           JSON.stringify(parsedNotes, null, 4),
+//           (writeErr) =>
+//             writeErr
+//               ? console.error(writeErr)
+//               : console.info('Successfully updated reviews!')
+//         );
+//       }})});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const htmlRoute = require('./routes/htmlroutes');
 const apiRoute = require('./routes/apiroutes');
+const fs = require('fs');
 
 const app = express();
 
@@ -19,6 +20,65 @@ app.use('/api/notes',apiRoute);
 const data = require('./db/db.json');
 app.get('/api/notes', (req,res) =>
     res.status(200).json(data));
+
+// GET request for reviews
+app.get('/api/notes', (req, res) => {
+  // Send a message to the client
+  res.status(200).json(`${req.method} request received to get notes`);
+
+  // Log our request to the terminal
+  console.info(`${req.method} request received to get notes`);
+});
+
+// POST request to add a review
+app.post('/api/notes', (req, res) => {
+  // Log that a POST request was received
+  console.log(req.body);
+
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+    };
+    // Obtain existing reviews
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        // Convert string into JSON object
+        const parsedNotes = JSON.parse(data);
+
+        // Add a new review
+        parsedNotes.push(newNote);
+
+        // Write updated reviews back to the file
+        fs.writeFile(
+          './db/db.json',
+          JSON.stringify(parsedNotes, null, 4),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully updated notes!')
+        );
+      }
+    });
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in posting review');
+  }
+});
 
 
 //LISTENING
